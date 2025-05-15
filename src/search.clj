c7a90a706ed1cdecc47f9f987b8cfadf4f4f423c
Andrea Crotti <andrea.crotti.0@gmail.com>
2025-05-15 09:37:07 +0100

    add medley and a few more tests

--- src/search.clj
+++ src/search.clj
@@ -1,10 +1,31 @@
-(ns search)
+(ns search
+  (:require
+   [medley.core :as m]
+   [clojure.string :as string]))
+
+(defonce index (atom {}))
+
+(defn reset-index!
+  []
+  (reset! index {}))
+
+(defn get-words-frequencies
+  [text doc-id]
+  (->> text
+       (re-seq #"\w+")
+       (map string/lower-case)
+       frequencies
+       (m/map-vals (fn [freq] {doc-id freq}))))
 
 (defn ingest
-  [text doc-id])
+  [text doc-id]
+  (let [freqs (get-words-frequencies text doc-id)
+        my-merge #(merge-with merge % freqs)]
+    (swap! index my-merge)))
 
 (defn search
-  [text])
+  [word]
+  (get @index word {}))
 
 (defn -main [& args]
   )
--- test/search_test.clj
+++ test/search_test.clj
@@ -2,10 +2,35 @@
   (:require [clojure.test :refer [deftest testing is]]
             [search]))
 
-(def sample-text "Pork ribs are cut from the neck fillet and are always juicy and tasty however you cook them. Serve this for dinner with summer greens and brown rice noodles, or with chips or new potatoes, perhaps with a bit of chilli sauce on the side. They’re easiest to eat if you abandon your cutlery and use your hands.")
+(def pork-ribs "Pork ribs are cut from the neck fillet and are always juicy and tasty however you cook them. Serve this for dinner with summer greens and brown rice noodles, or with chips or new potatoes, perhaps with a bit of chilli sauce on the side. They’re easiest to eat if you abandon your cutlery and use your hands.")
 
+(def pork-larb "Also called larb or laap, this pork dish has a beautiful balance of freshness, crunch and spiciness. There are quite a few ingredients, but preparing and assembling them is quick and simple. You could also add a little cooked diced potato or butternut squash. Little Gem is particularly good for scooping up the meat, but any other lettuce you'd like will also be fine. This is often served with a mango salad.")
+
+(defmacro with-data
+  [docs & body]
+  `(do
+    (search/reset-index!)
+    (doseq [[text# doc-id#] ~docs]
+      (search/ingest text# doc-id#))
+    ~@body))
 
 (deftest search-test
-  (testing "Can find matches"
-    (search/ingest sample-text "doc1.txt")
-    (is (= (search/search "pork") {"doc1.txt" 1}))))
+  (testing "Word not found"
+    (with-data [[pork-ribs "doc1.txt"]]
+      (is (= {} (search/search "missing")))))
+
+  (testing "Can find matches from one word"
+    (with-data [[pork-ribs "doc1.txt"]]
+      (is (= {"doc1.txt" 1}
+             (search/search "pork")))))
+
+  (testing "Can find matches from multiple words"
+    (with-data [[pork-ribs "doc1.txt"]]
+      (is (= {"doc1.txt" 1}
+             (search/search "pork ribs")))
+      (is (= {"doc1.txt" (/ 1 2)}
+             (search/search "pork missing") ))))
+
+  (testing "Ingesting multiple documents"
+    (with-data [[pork-ribs "doc1.txt"] [pork-larb "doc2.txt"]]
+      (is (= {} (search/search "pork"))))))
